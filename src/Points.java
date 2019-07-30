07d33b79c3661423947151072508c8f7c63f20bb
WilliamCadenas21 <williamcadenas21@gmail.com>
2019-07-29 22:02:50 -0500

    Script finalizado, todavia se puede mejorar

--- src/Points.java
+++ src/Points.java
@@ -1,28 +1,20 @@
-/*
- * Ejemplo básico en Java2D
- * 
- * Tomado de el Tutorial de Java2D de ZetTutorial: http://zetcode.com/tutorials/java2dtutorial/
- * 
- * Java tiene un tutorial oficial para Java2D:   
- */
-
-//package points;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
-import java.util.*;
+import java.awt.Point;
 
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 
-import java.util.Random;
-
 public class Points extends JPanel {
-
-    private final int pixelSize = 3;
-    private final double angulo = 30;
+    
+    //varaibles privadas con las caules funciona el algoritmo
+    private final double angulo = 20;
+    private final int steps = 1;
+    private int R = 200;
+    private int numRombos = 7;
 
     /*
    * En esta función se dibuja.
@@ -50,20 +42,39 @@ public class Points extends JPanel {
         g2d.drawLine(0, h / 2, w, h / 2);// eje x
         g2d.drawLine(w / 2, 0, w / 2, h);// eje y
 
+        Point[] points = new Point[4];
+        points[0] = new Point(R, 0);
+        points[1] = new Point(0, R);
+        points[2] = new Point(-R, 0);
+        points[3] = new Point(0, -R);
+
+        //dibujado del primer rombo
         g2d.setColor(Color.blue);
-        //octante(0, 0, 100, 50, g2d, w, h, 3);
+        drawLine(g2d, R, 0, 0, R, w, h);
+        drawLine(g2d, 0, R, -R, 0, w, h);
+        drawLine(g2d, -R, 0, 0, -R, w, h);
+        drawLine(g2d, 0, -R, R, 0, w, h);
         
-        int num = 100;
+        double ang;
+        int newY = 0, newX = 0;
+        g2d.setColor(Color.red);
+        for (int j = 0; j < numRombos; j++) {
+            ang =(j+1)*angulo;
+            //calcula los nuevos puntos rotados
+            for (int i = 0; i < 4; i++) {
+                newY = (int) Math.round(R * Math.sin(Math.toRadians(ang)));
+                newX = (int) Math.round(R * Math.cos(Math.toRadians(ang)));
+                points[i] = new Point(newX, newY);
+                ang = ang + 90;
+            }
+            //Dibuja las lineas entre los nuevos puntos
+            for (int k = 0; k < 3; k++) {
+                drawLine(g2d, points[k].x, points[k].y, points[k + 1].x, points[k + 1].y, w, h);
+            }
+            drawLine(g2d, points[3].x, points[3].y, points[0].x, points[0].y, w, h);
+        }
         
-        double value = Math.sin(angulo);
-        
-        drawLine(g2d, num, 0, 0, num, w, h);
-        drawLine(g2d, -num, 0, 0, num, w, h);
-        drawLine(g2d, -num, 0, 0, -num, w, h);
-        drawLine(g2d, num, 0, 0, -num, w, h);
-        
-        //g2d.setColor(Color.black);
-        //octante(0,0,100,50,g2d,w,h,10);
+        System.out.println("Finalizo el dibujo");
     }
 
     private void drawLine(Graphics g, int x1, int y1, int x2, int y2, int w, int h) {
@@ -76,20 +87,18 @@ public class Points extends JPanel {
         int dx2 = 2 * dx; // slope scaling factors to
         int dy2 = 2 * dy; // avoid floating point
 
-        int ix = x1 < x2 ? 1 : -1; // increment direction
-        int iy = y1 < y2 ? 1 : -1;
+        int ix = x1 < x2 ? steps : -steps; // increment direction
+        int iy = y1 < y2 ? steps : -steps;
 
         int x = x1;
         int y = y1;
 
         if (dx >= dy) {
             while (true) {
-                //plot(g, x, y);
-                
                 int xj = w / 2 + x; //transformar la cordenada en x     
                 int yj = h / 2 - y; //transformar la cordenada en y
                 g.drawLine(xj, yj, xj, yj); // dibuja el punto
-                
+
                 if (x == x2) {
                     break;
                 }
@@ -102,11 +111,10 @@ public class Points extends JPanel {
             }
         } else {
             while (true) {
-                //plot(g, x, y);
                 int xj = w / 2 + x; //transformar la cordenada en x     
                 int yj = h / 2 - y; //transformar la cordenada en y
                 g.drawLine(xj, yj, xj, yj); // dibuja el punto
-                
+
                 if (y == y2) {
                     break;
                 }
@@ -116,56 +124,6 @@ public class Points extends JPanel {
                     x += ix;
                     d -= dy2;
                 }
-            }
-        }
-    }
-
-    private void plot(Graphics g, int x, int y) {
-        int w = (getWidth() - 1) / pixelSize;
-        int h = (getHeight() - 1) / pixelSize;
-        int maxX = (w - 1) / 2;
-        int maxY = (h - 1) / 2;
-
-        int borderX = getWidth() - ((2 * maxX + 1) * pixelSize + 1);
-        int borderY = getHeight() - ((2 * maxY + 1) * pixelSize + 1);
-        int left = (x + maxX) * pixelSize + borderX / 2;
-        int top = (y + maxY) * pixelSize + borderY / 2;
-
-        g.setColor(Color.black);
-        //g.drawOval(left, top, pixelSize, pixelSize);
-
-        g.drawLine(top, left, top, left);
-    }
-
-    public static void octante(int x1, int y1, int x2, int y2, Graphics2D g2d, int w, int h, int steps) {
-        int xj, yj;
-
-        //primer paso calcular el delta
-        int dy = y2 - y1;
-        int dx = x2 - x1;
-
-        //segundo paso calcular los incrementos
-        int incE = 2 * dy;
-        int incNE = 2 * dy - 2 * dx;
-
-        // se le da el valor inicial a delta, y
-        int d = 2 * dy - dx;
-        int y = y1;
-
-        // se procede a realizar el recorrido por el eje de las x
-        for (int x = x1; x < x2; x = x + steps) {
-
-            xj = w / 2 + x; //transformar la cordenada en x     
-            yj = h / 2 - y; //transformar la cordenada en y
-
-            g2d.drawLine(xj, yj, xj, yj); // dibuja el punto
-
-            //transforma el delta y y segun el valor de d
-            if (d <= 0) {
-                d += incE;
-            } else {
-                d += incNE;
-                y += steps;
             }
         }
     }
